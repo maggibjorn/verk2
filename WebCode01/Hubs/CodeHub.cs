b32d6527ce1ddc6d83914b54516d6a35074d7fe2
maggibjorn <mb14@ru.is>
2017-05-08 15:10:28 +0000

    file saves on editor change

--- WebCode01/Hubs/CodeHub.cs
+++ WebCode01/Hubs/CodeHub.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using WebCode01.Services;
+using WebCode01.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace WebCode01.Hubs
 {
     public class CodeHub : Hub
     {
-        public void OnChange(object changeData, int documentId)
+        public ProjectFilesService service = new ProjectFilesService();
+        public void OnChange(object changeData, int documentId, string content)
         {
             //Clients.All.OnChange(changeData);
             Clients.Group(Convert.ToString(documentId), Context.ConnectionId).OnChange(changeData);
+            
+            ProjectFileViewModel model = new ProjectFileViewModel
+            {
+                id = documentId,
+                fileContent = content
+            };
+            service.saveCodeToDb(model); // Save editor changes to database
         }
 
         public void JoinDocument(int documentId)
