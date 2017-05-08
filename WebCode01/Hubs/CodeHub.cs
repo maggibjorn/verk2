637de00a2b301d4526a081213c87a72c05858f7e
maggibjorn <mb14@ru.is>
2017-05-08 13:15:08 +0000

    signal r added

---
+++ WebCode01/Hubs/CodeHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebCode01.Hubs
+{
+    public class CodeHub : Hub
+    {
+        public void OnChange(object changeData, int documentId)
+        {
+            //Clients.All.OnChange(changeData);
+            Clients.Group(Convert.ToString(documentId), Context.ConnectionId).OnChange(changeData);
+        }
+
+        public void JoinDocument(int documentId)
+        {
+            Groups.Add(Context.ConnectionId, Convert.ToString(documentId));
+        }
+    }
+}
--- WebCode01/Startup.cs
+++ WebCode01/Startup.cs
@@ -9,6 +9,7 @@ namespace WebCode01
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
